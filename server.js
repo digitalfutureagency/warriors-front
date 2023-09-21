d69d0a5fb3a39bd81bc0986ecf6ead453b2c457b
Jonathan Molina <jonathan.molina@hrinteractive.co>
2023-09-21 10:44:30 -0500

    deploy #2

---
+++ server.js
@@ -0,0 +1,21 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+const frameguard = require("frameguard");
+
+// Don't allow me to be in ANY frames:
+app.use(frameguard({ action: "deny" }));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
